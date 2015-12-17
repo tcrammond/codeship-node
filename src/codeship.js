36bc33b0c58ef10e8fed7cc74986ad9b2d5009f9
Tyler Crammond <tylerchc92@gmail.com>
2015-12-17 22:26:48 +0000

    Add list projects method

--- src/codeship.js
+++ src/codeship.js
@@ -1,9 +1,33 @@
+var request = require('request');
+
 class Codeship {
-    constructor({apiKey} = {}) {
-        if (!apiKey) {
-            throw new Error('A Codeship API key must be passed to the constructor.');
-        }
+  constructor({apiKey} = {}) {
+    if (!apiKey) {
+      throw new Error('A Codeship API key must be passed to the constructor.');
     }
+
+    this._apiKey = apiKey;
+    this._baseUrl = 'https://codeship.com/api/v1';
+
+    this.projects = {};
+    this.projects.list = (cb) => {
+      this._performRequest('/projects.json', (err, body) => {
+        cb(err, body.projects);
+      });
+    };
+
+  }
+
+  _performRequest(path, cb, method = 'GET') {
+    request({
+      url: `${this._baseUrl}${path}?api_key=${this._apiKey}`,
+      method: method,
+      json: true
+    }, (err, response, body) => {
+      cb(err, body);
+    });
+  }
 }
+
 
 module.exports = Codeship;
--- test/codeship.spec.js
+++ test/codeship.spec.js
@@ -1,18 +1,58 @@
+var nock = require('nock');
+
 var Codeship = require('../lib/codeship');
 
 describe('Codeship module', function() {
+  var baseUrl = 'https://codeship.com/api/v1';
+  var apiKey;
 
-    var apiKey;
+  beforeEach(function() {
+    apiKey = '12345';
+  });
 
-    beforeEach(function() {
-        apiKey = '12345';
+  describe('instantiation', function() {
+    it('throws an error if an API key is not provided', function() {
+      expect(function() {new Codeship()}).toThrow(new Error('A Codeship API key must be passed to the constructor.'));
     });
 
-    describe('instantiation', function() {
+    it('sets the API key property to the specified API key', function() {
+      var codeship = new Codeship({apiKey: apiKey});
+      expect(codeship._apiKey).toBe(apiKey);
+    });
+  });
 
-        it('throws an error if an API key is not provided', function() {
-            expect(function() {new Codeship();}).toThrow(new Error('A Codeship API key must be passed to the constructor.'));
+  describe('projects', function() {
+    var projectsResource = '/projects.json';
+    var projects = [
+      {id: 1},
+      {id: 2}
+    ];
+
+    describe('list', function() {
+      it('retrieves and returns available projects', function(done) {
+        nock(baseUrl)
+          .get(projectsResource + '?api_key=' + apiKey)
+          .reply(200, {
+            projects: projects
+          });
+
+        makeCodeship().projects.list(function(err, data) {
+          if (!nock.isDone()) {
+            fail();
+          }
+
+          expect(data).toEqual(projects);
+          done();
         });
+      });
+    });
+
+    describe('get', function() {
 
     });
+  });
+
+  function makeCodeship() {
+    return new Codeship({apiKey: apiKey});
+  }
 });
