576c7bdb7b994509714dfff177e8b2ad453392c5
Tyler Crammond <tylerchc92@gmail.com>
2015-12-17 20:14:55 +0000

    Initial commit. Create codeship class, set up babel and npm commands.

---
+++ src/codeship.js
@@ -0,0 +1,9 @@
+class Codeship {
+    constructor({apiKey}) {
+        if (!apiKey) {
+            throw new Error('A Codeship API key must be passed to the constructor.');
+        }
+    }
+}
+
+module.exports = Codeship;
